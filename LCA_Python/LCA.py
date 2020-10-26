46c2797f52cff8524de6f84eada8c137d6a4291e
miocalla <61199986+miocalla@users.noreply.github.com>
2020-10-26 10:18:40 +0000

    updated LCA class
    removed main line

--- LCA_Python/LCA.py
+++ LCA_Python/LCA.py
@@ -1,16 +1,16 @@
 '''
 Created on 13 Oct 2020
 
+# GeeksforGeeks implementation
 @author: michaelocallaghan
 '''
 class Node: 
-    # Constructor 
+    
     def __init__(self, key): 
         self.key =  key 
         self.left = None
         self.right = None
   
-# Finds the path from root node to given root of the tree. Returns true if path exists, otherwise false 
 def findPath( root, path, k): 
   
     if root is None: 
@@ -23,18 +23,17 @@ def findPath( root, path, k):
     if root.key == k : 
         return True
   
-    # Check if k is found in left or right sub-tree 
+    # Check if in left or right sub-tree 
     if ((root.left != None and findPath(root.left, path, k)) or
             (root.right!= None and findPath(root.right, path, k))): 
         return True 
   
-    # If not present in subtree rooted with root, remove root from path and return False 
-       
+    # If it gets this far, it means its not present so lets remove the root from the path and return False
     path.pop() 
     return False
   
 # Returns LCA if node number1 , number2 are present in the given binary tree otherwise return -1 
-def findLCA(root, number1, number2): 
+def findLowestCommonAncestor(root, number1, number2): 
   
     # To store paths from root to number1 and number2
     path1 = [] 
@@ -52,16 +51,5 @@ def findLCA(root, number1, number2):
         i += 1
     return path1[i-1] 
   
-root = Node(1) 
-root.left = Node(2) 
-root.right = Node(3) 
-root.left.left = Node(4) 
-root.left.right = Node(5) 
-root.right.left = Node(6) 
-root.right.right = Node(7) 
-  
-print (("LCA(4, 5) = %d") %(findLCA(root, 4, 5,)))
-print (("LCA(4, 6) = %d") %(findLCA(root, 4, 6))) 
-print (("LCA(3, 4) = %d") %(findLCA(root,3,4)))
-print (("LCA(2, 4) = %d") %(findLCA(root,2, 4)))
+
 
